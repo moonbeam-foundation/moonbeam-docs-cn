403fa88c57764655e48ea13ab7993f669c4befc9
Erin Shaben <eshaben@icloud.com>
2024-05-23 22:42:57 -0400

    remove code snippets

--- .snippets/code/builders/integrations/oracles/supra/ISupraSValueFeed.sol
+++
@@ -1,6 +0,0 @@
-interface ISupraSValueFeed {
-    function getSvalue(uint64 _pairIndex) external view returns (bytes32, bool);
-    function getSvalues(
-        uint64[] memory _pairIndexes
-    ) external view returns (bytes32[] memory, bool[] memory);
-}
